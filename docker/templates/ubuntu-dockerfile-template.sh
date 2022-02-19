b715583968b89517d1e5a6151a4fe88aa886226f
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-19 21:13:52 +0000

    apt-get instead of apt

--- docker/templates/ubuntu-dockerfile-template.sh
+++ docker/templates/ubuntu-dockerfile-template.sh
@@ -3,5 +3,5 @@
 tag=$1
 
 echo "FROM ubuntu:$tag"
-echo "RUN apt update && apt install -y gcc build-essential"
+echo "RUN apt-get update && apt-get install -y gcc build-essential"
 echo "ENTRYPOINT [ \"bash\" ]"
