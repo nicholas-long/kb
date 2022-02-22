cef4026c6c93cc81f7df468d41bb027da0292199
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-22 07:53:41 +0000

    hash a git repository

---
+++ bash-scripting/hash_git_repository.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function walk_tree {
+  tag=$2
+  path=$3
+  git cat-file -p $1 | \
+    while read asdf tp hash file; do
+      [ $tp = "tree" ] && walk_tree $hash $tag "$path/$file" # recursion
+      [ $tp = "blob" ] && printf "$hash\t$tag\t$path/$file\r\n"
+    done
+}
+
+function walk_commit {
+  tag=$2
+  git cat-file -p $1 | head -n 2 | grep tree | awk '{print $2}' | while read tree; do walk_tree $tree $tag ""; done
+}
+
+function walk_commits_tags {
+  while read commit tag; do
+    walk_commit $commit $tag
+  done
+}
+
+# clone repo into temp dir
+tf=$(mktemp)
+rm $tf && mkdir $tf
+git clone --mirror https://github.com/skanehira/docui $tf
+rm /tmp/hash_list 2>/dev/null
+cd $tf
+
+# get all tags and start walking their commits
+cat packed-refs | grep refs/tags/ | walk_commits_tags | tee /tmp/hash_list
+echo
+
+# take only unique git hashes and do md5 hashes of those files
+cat /tmp/hash_list | awk -F "\t" '{print $1}' | sort -u | \
+  while read githash; do
+    echo $githash
+    git cat-file -p $githash | md5sum | awk '{print $1}'
+  done | paste - -
+
+rm -rf $tf
