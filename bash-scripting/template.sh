0cee97ca2e5d739c3a40cc94f408ecd251c446ef
coyote <nicholas.eden.long@gmail.com>
2022-02-26 15:45:53 -0600

    update template

--- bash-scripting/template.sh
+++ bash-scripting/template.sh
@@ -2,3 +2,26 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# parse args
+PAGING="no"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--paging)
+      PAGING=1
+      ;;
+    -o|--optional)
+      shift # pop arg
+      echo "Optional parameter $1"
+      ;;
+    *)
+      KB_DIR="$1"
+      ;;
+  esac
+  shift
+done
+
+# exit if parameter is missing
+test -z "$KB_DIR" && echo "Missing parameter" && exit 1
+
+echo "Positional parameter $KB_DIR"
+echo "Paging $PAGING"
