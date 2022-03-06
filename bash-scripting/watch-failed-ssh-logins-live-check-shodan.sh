9a5e9f7c70fe810206d25c25f5c92d42a65bb940
Nicholas Long <nicholas.eden.long@gmail.com>
2022-03-06 02:29:12 +0000

    all in one awk script

--- bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
+++ bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-mkdir /tmp/shodancache
-
 # watch failed ssh login attempts as a live stream
 # lookup unique hosts with shodan api
+
 #cat /var/log/auth.log | \
 tail -f /var/log/auth.log | \
   awk 'match($0, /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/) {
+    ip = substr($0, RSTART, RLENGTH)
+    if (seen[ip]) {
+      print "Already saw " ip
+    } else {
+      ip = substr($0, RSTART, RLENGTH)
+      print ip
+      seen[ip] = 1
+      system("shodan host " ip)
+    }
+  }
+  match($0, /Invalid user (.+)/) {
     print substr($0, RSTART, RLENGTH)
-    fflush()
-  }' | \
-  while read ip; do
-    echo "$ip"
-    [ -f "/tmp/shodancache/$ip" ] || (sleep 2 && shodan host "$ip" && sleep 2 && shodan host "$ip" > /tmp/shodancache/$ip)
-    bat --paging=never --language=markdown "/tmp/shodancache/$ip"
-  done
-  # filter unique
-  #awk '!seen[$0] {print; fflush()} {++seen[$0]}' | \
+  }'
