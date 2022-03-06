55374ba7f4fbde52957b92a3f13998b5ed677a57
Nicholas Long <nicholas.eden.long@gmail.com>
2022-03-06 01:18:48 +0000

    another log message to add

--- bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
+++ bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
@@ -7,6 +7,7 @@ tail -f /var/log/auth.log | \
   awk '/\]: Connection closed by/ { print $9 ; fflush() }
     /Connection closed by invalid user/ { print $12 ; fflush() }
     /Disconnected from invalid user/ { print $11 ; fflush() }
+    /Connection reset by/ { print $9 ; fflush() }
     /Invalid user .+ from/ { print $10; fflush() } ' | \
   awk '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ { print; fflush() }' | \
   while read ip; do
