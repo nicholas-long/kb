854b19a8284fb0d7e4c8b40b3f03f067b37b97d8
Nicholas Long <nicholas.eden.long@gmail.com>
2022-03-06 01:42:26 +0000

    even simpler. cache shodan results.

--- bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
+++ bash-scripting/watch-failed-ssh-logins-live-check-shodan.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+mkdir /tmp/shodancache
+
 # watch failed ssh login attempts as a live stream
 # lookup unique hosts with shodan api
 #cat /var/log/auth.log | \
 tail -f /var/log/auth.log | \
-  awk '/\]: Connection closed by/ { print $9 ; fflush() }
-    /Connection closed by invalid user/ { print $12 ; fflush() }
-    /Disconnected from invalid user/ { print $11 ; fflush() }
-    /Connection reset by/ { print $9 ; fflush() }
-    /Invalid user .+ from/ { print $10; fflush() } ' | \
-  awk '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ { print; fflush() }' | \
+  grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | \
   while read ip; do
     echo "$ip"
-    shodan host "$ip"
-    sleep 2
+    [ -f "/tmp/shodancache/$ip" ] || (shodan host "$ip" > /tmp/shodancache/$ip && sleep 2)
+    bat --paging=never --language=markdown "/tmp/shodancache/$ip"
   done
   # filter unique
   #awk '!seen[$0] {print; fflush()} {++seen[$0]}' | \
