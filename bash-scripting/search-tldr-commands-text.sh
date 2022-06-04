ec158a720f0bee59419fc06fe844fb5e2c472282
Nicholas Long <nicholas.eden.long@gmail.com>
2022-06-04 05:36:29 +0000

    improvements to tldr search

--- bash-scripting/search-tldr-commands-text.sh
+++ bash-scripting/search-tldr-commands-text.sh
@@ -3,6 +3,6 @@
 # search through tldr command descriptions with fzf and display file with bat
 
 find ~/.local/share/tldr/tldr/pages -type f -name '*.md' | \
-  xargs awk 'BEGIN { OFS="\t" } /^# / { h = $2 } /^- / { print FILENAME,FNR,h,$0 }' | \
+  xargs awk 'BEGIN { OFS="\t" } /^# / { h = $2 } /^- / || /^> / { print FILENAME,FNR,h,$0 }' | \
   fzf | \
-  awk '{system("bat " $1) }'
+  awk -F $'\t' '{system("bat " $1) }'
