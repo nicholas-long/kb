e444d98ff096477d0981294ba8f9ab77a60fdb99
Nicholas Long <nicholas.eden.long@gmail.com>
2022-07-21 04:34:34 +0000

    clean this up - it's messing up bash scripts

--- linux/runneo4j.sh
+++ linux/runneo4j.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# run neo4j in a docker
+
 docker run --rm \
     -p7474:7474 -p7687:7687 \
     --env NEO4J_AUTH=neo4j/test \
     neo4j:latest
 
-#    -v $HOME/neo4j/data:/data \
-#    -v $HOME/neo4j/logs:/logs \
-#    -v $HOME/neo4j/import:/var/lib/neo4j/import \
-#    -v $HOME/neo4j/plugins:/plugins \
+# mount host neo4j data into docker
+##    -v $HOME/neo4j/data:/data \
+##    -v $HOME/neo4j/logs:/logs \
+##    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+##    -v $HOME/neo4j/plugins:/plugins \
