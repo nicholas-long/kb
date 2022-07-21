426d3df72b83a9df1deba9601cd3358470ef5a46
Nicholas Long <nicholas.eden.long@gmail.com>
2022-07-21 03:18:52 +0000

    nmap ports and service names

---
+++ awk-scripting/nmap-ports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# query nmap ports and service description definitions
+
+awk '
+BEGIN { OFS = "\t" }
+/^#/ { next }
+$1 == "unknown" { next }
+{
+  split($2,arr,"/")
+  port=arr[1]
+  proto=arr[2]
+  print port, proto, $1
+}
+' /usr/share/nmap/nmap-services
