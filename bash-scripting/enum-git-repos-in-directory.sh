65a117f74f9f130fe414adc016fa71bd6e7e830a
Nicholas Long <nicholas.eden.long@gmail.com>
2022-08-10 23:35:07 +0000

    fix keywords

--- bash-scripting/enum-git-repos-in-directory.sh
+++ bash-scripting/enum-git-repos-in-directory.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# find all the git repositories in a directory and remove .git from the name
+# find all git repositories in a directory
+# remove git from directory name
 find "$1" -name .git -type d | awk 'BEGIN {FS = OFS = "/" } { NF--;print }'
