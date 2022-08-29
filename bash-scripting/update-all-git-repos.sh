4380b55a223ab44ef5391fa0a673f8a109553b10
Nicholas Long <nicholas.eden.long@gmail.com>
2022-08-28 21:55:47 -0500

    update git repos interactively

---
+++ bash-scripting/update-all-git-repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# update all the git repos in home direcotry and prompt with lazygit
+
+~/kb/bash-scripting/enum-git-repos-in-directory.sh $HOME |\
+  awk -F / 'NF == 4 {print}' |\
+  while read dir; do
+    cd $dir
+    diffcount=$(git status --porcelain | wc -l)
+    [ $diffcount -ne 0 ] && lazygit
+  done
