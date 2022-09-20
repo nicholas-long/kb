513df68a2032c5689be0528950f92b2a0f0df15c
Nicholas Long <nicholas.eden.long@gmail.com>
2022-09-20 05:32:13 +0000

    convert burp saved request into python requests script

---
+++ bash-scripting/copy-burp-request-as-python-script.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+xpup="$HOME/go/bin/xpup"
+#go install github.com/ericchiang/xpup@latest
+file="$1"
+http_request=$(mktemp)
+
+# parse burpsuite saved request file and convert to python requests script for copying
+cat login.req | ~/go/bin/xpup '//request' | base64 -d > $http_request
+dos2unix $http_request 2>/dev/null
+
+url=$(cat $file | $xpup //url)
+ct=$(awk '/^Content-Type/ { print $2 }' $http_request)
+method=$(awk 'NR == 1 { print $1 }' $http_request)
+getparams=$(echo $url | ~/kb/python/get-url-params.py)
+http_headers=$(awk '
+  BEGIN {
+    FS = ": "
+    OFS = "\t"
+  }
+  /^Cookie/ { next }
+  /^Host/ { next }
+  /^Content-Length/ { next }
+  /^Connection/ { next }
+  /^Upgrade-Insecure-Requests/ { next }
+  /^Accept/ { next }
+  /^Content-Type/ { next }
+  NF == 2 { $1 = $1; print }
+  /^$/ { exit 0 }
+  ' $http_request | ~/kb/python/tsv-to-python-dict.py
+)
+
+cookies=$(awk '
+/^Cookie/ {
+  gsub(/;/,"")
+  for (n = 2; n <= NF; n++)
+    print $n
+}
+' $http_request | ~/kb/python/decode-cookies.py)
+
+#--------------------------------------------------------------------------------
+
+cat << HEADER
+import requests
+
+url = "$url"
+cookies = $cookies
+
+## optional
+getparams = $getparams
+headers = $http_headers
+
+HEADER
+
+if [ $method == "POST" ]; then
+  if [ $ct == "application/x-www-form-urlencoded" ]; then
+    postdata=$(~/kb/awk-scripting/get-http-post-content.awk $http_request | ~/kb/python/decode-post-params.py)
+
+    cat << PYTHON
+postdata = $postdata
+r = requests.post(url, data=postdata, cookies=cookies)
+PYTHON
+  fi
+fi
+
+cat << FOOTER
+print(r.text)
+FOOTER
+
+rm $http_request
